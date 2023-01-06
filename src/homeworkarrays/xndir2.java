a171d10efc827302629c1979089d80c97b171a36
Muradyan-Narine <narine.muradyan@mail.ru>
2023-01-06 23:20:03 +0300

    ьь

--- src/homeworkarrays/xndir2.java
+++ src/homeworkarrays/xndir2.java
@@ -2,8 +2,13 @@ package homeworkarrays;
 
 public class xndir2 {
     public static void main(String args[]){
-    char chars={'j','a','v','a','l','o','v','e',};
-System.out.println(chars);
+    char[] chars={'j','a','v','a','l','o','v','e'};
+ int k=0;
+ k= chars.length;
+ k=k/2;
+ System.out.println(chars[k-1]);
+ System.out.println(chars[k]);
+
 
     }
 }
