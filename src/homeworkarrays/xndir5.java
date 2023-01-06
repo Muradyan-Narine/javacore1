f8dfa8702f82e4346f823c7ae41735e2fd8d2575
Muradyan-Narine <narine.muradyan@mail.ru>
2023-01-06 23:43:08 +0300

    ьь

---
+++ src/homeworkarrays/xndir5.java
@@ -0,0 +1,12 @@
+package homeworkarrays;
+
+public class xndir5 {
+    public static void main(String args[]){
+        char[] text = {' ',' ','b','a','r','e','v',' ',' '};
+        int i;
+        for (i=0;i< text.length;i++){
+            if (text[i]!=' ')
+                System.out.print(text[i]);
+ }
+    }
+}
