0093761b0fd0d46b1e2efd73a1a66a3792f70846
Muradyan-Narine <narine.muradyan@mail.ru>
2023-01-06 23:44:03 +0300

    ьь

---
+++ src/homeworkarrays/xndir4.java
@@ -0,0 +1,14 @@
+package homeworkarrays;
+
+public class xndir4 {
+    public static void main (String args[]){
+        char[] chars3={'j','a','v','a','l','o','l','y'};
+        int k=0;
+        k=chars3.length;
+        if (chars3[k-1]=='y'& chars3[k-2]=='l')
+            System.out.println("true");
+
+    }
+
+
+}
